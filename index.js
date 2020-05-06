e9086f769317cf36b5cf1cef2b7d8ba1f6c5ff0c
Shayan Mohsin <shaymotionfilms@gmail.com>
2020-05-06 03:07:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // Add your code here
-let submitData = {
-  userName: "Shayan",
-  userEmail: "shaymotionfilms@gmail.com"};
-  
-  
+
+function submitData(userName, userEmail){
+
+
+    let configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject).then(function(response) {
+        return response.json();
+    })
+    .then(function (object) {
+        document.body.innerHTML = object[ "id" ]
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error.message
+    })
+
+}
